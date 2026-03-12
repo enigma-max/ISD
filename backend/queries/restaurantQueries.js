bb2b408b1f30ac1b857b3d82f55a6e68df57f530
Fariha Ifrat <ifratfariha513@email.com>
2026-03-12 12:51:30 +0600

    Add backend restaurant search and suggestion

---
+++ backend/queries/restaurantQueries.js
@@ -0,0 +1,25 @@
+//for search
+export const getRestaurantsQuery = `
+SELECT
+  r.restaurant_id,
+  r.name,
+  r.cuisine_type,
+  r.pricing,
+  r.logo_url,
+  COALESCE(AVG(rt.rating),0) as avg_rating
+FROM restaurant r
+LEFT JOIN rating rt
+  ON rt.restaurant_id = r.restaurant_id
+WHERE r.name ILIKE $3
+GROUP BY r.restaurant_id
+ORDER BY r.name
+LIMIT $1 OFFSET $2
+`
+// for search suggestions
+export const getRestaurantSuggestionsQuery = `
+SELECT name
+FROM restaurant
+WHERE name ILIKE $1
+ORDER BY name
+LIMIT 10
+`
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import restaurantRoutes from './routes/restaurantRoutes.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors()); // allow frontend requests
 app.use(express.json());
 
 app.use('/api/restaurants', restaurantRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
